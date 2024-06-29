f59e15409722b807b9e06034b95b752be4b8bef5
mmsmesut <m.mesutsincar@hotmail.com>
2024-06-29 08:37:08 +0300

    Add project files.

---
+++ MShop.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MShop.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //BaseURl/Home/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ MShop.Presentation/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Mvc.1
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+//Mvc.2
+app.UseRouting();
+//Mvc.3
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}"); //3
+
+app.Run();
