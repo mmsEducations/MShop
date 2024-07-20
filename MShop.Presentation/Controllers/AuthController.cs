64e8665c9c027d673b2e3f3658887f9dd293668e
mmsmesut <m.mesutsincar@hotmail.com>
2024-07-20 12:45:01 +0300

    Repositry dsign pattern and admin entegration

---
+++ MShop.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿namespace MShop.Presentation.Controllers
+{
+    public class AuthController : Controller
+    {
+        //Giriş-login
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        //Kayıtol-register
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
